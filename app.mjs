8f96c59a558d1fba57c27f5e173ddae03ad04e36
Ariole David <>
2022-07-21 20:43:37 +0000

    nothing

--- app.mjs
+++ app.mjs
@@ -10,8 +10,32 @@ function startApp() {
     if(header) {
       header.textContent = 'make some magic here!!';
     }
-  };
-  
+  }
+//adesola busari
+  function ToBeSeparated(words){
+  var Separated =[]
+
+    for (var i=0; i < words.length; i++){
+      varStr = words[1].trim()
+    if (str.indexOf('')>-1)
+      Separated =Separated.Concat(Str.Split(''));
+    else
+      Separated.Push(Str);
+    }
+    return Separated;
+}
+console.log(ToBeSeparated( ))
+// Boluwatife Morenikeji
+  function redactNow(text, toBeScrambled, rePlacer) {
+    let wordScanned = text.split(" ").length
+    let newText = text;
+    const redactArray = toBeScrambled.split(" ")
+      for (let i = 0; i < redactArray.length; i++) {
+      newText = newText.replaceAll(redactArray[i], rePlacer ? rePlacer : "***")
+    }
+    return {Result: newText, numberOfWordsScanned: wordScanned}
+  }
+
   // ======= DO NOT EDIT ============== //
   export default startApp;
   // ======= EEND DO NOT EDIT ========= //
