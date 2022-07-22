96f3ece86bbb61dbc3d9d85ef977a9769216fbf9
David Ariole <davidariole@gmail.com>
2022-07-22 01:55:25 +0100

    code review

--- app.mjs
+++ app.mjs
@@ -1,3 +1,7 @@
+import { redactNow } from "./helper.mjs";
+
+
+
 function startApp() {
     // Your entire app should not necessarily be coded inside this 
     // single function (though there's no penalty for that), 
@@ -11,30 +15,8 @@ function startApp() {
       header.textContent = 'make some magic here!!';
     }
   }
-//adesola busari
-  function ToBeSeparated(words){
-  var Separated =[]
 
-    for (var i=0; i < words.length; i++){
-      varStr = words[1].trim()
-    if (str.indexOf('')>-1)
-      Separated =Separated.Concat(Str.Split(''));
-    else
-      Separated.Push(Str);
-    }
-    return Separated;
-}
-console.log(ToBeSeparated( ))
-// Boluwatife Moronkeji
-  function redactNow(text, toBeScrambled, rePlacer) {
-    let wordScanned = text.split(" ").length
-    let newText = text;
-    const redactArray = toBeScrambled.split(" ")
-      for (let i = 0; i < redactArray.length; i++) {
-      newText = newText.replaceAll(redactArray[i], rePlacer ? rePlacer : "***")
-    }
-    return {Result: newText, numberOfWordsScanned: wordScanned}
-  }
+
 
   // ======= DO NOT EDIT ============== //
   export default startApp;
