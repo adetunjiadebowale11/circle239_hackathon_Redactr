05d5e5f65622819ae52cebdd5b5a37db7a904854
David Ariole <davidariole@gmail.com>
2022-07-23 19:38:41 +0100

    code review

--- helper.mjs
+++ helper.mjs
@@ -1,39 +1,16 @@
-// Boluwatife Moronkeji
-// function redactNow(text, toBeScrambled, rePlacer) {
-//     let wordScanned = text.split(" ").length
-//     let newText = text;
-//     const redactArray = toBeScrambled.split(" ")
-//       for (let i = 0; i < redactArray.length; i++) {
-//       newText = newText.replaceAll(redactArray[i], rePlacer ? rePlacer : "***")
-//     }
-//     return {Result: newText, numberOfWordsScanned: wordScanned}
-//   }
-
+//Boluwatife Moronkeji
 function redactNow(text, toBeScrambled, rePlacer) {
     let wordScanned = text.split(" ").length
     let newText = text;
     const redactArray = toBeScrambled.split(" ")
       for (let i = 0; i < redactArray.length; i++) {
-      newText = newText.replaceAll(redactArray[i], rePlacer ? rePlacer.repeat(redactArray[i].length) : "*".repeat(redactArray[i].length))
+      newText = newText.replaceAll(redactArray[i], rePlacer ? rePlacer : "***")
     }
     return {Result: newText, numberOfWordsScanned: wordScanned}
   }
 
+
 //adesola busari
-//   function ToBeSeparated(words){
-//   var Separated =[]
-
-//     for (var i=0; i < words.length; i++){
-//       varStr = words[1].trim()
-//     if (str.indexOf('')>-1)
-//       Separated =Separated.Concat(Str.Split(''));
-//     else
-//       Separated.Push(Str);
-//     }
-//     return Separated;
-// }
-// console.log(ToBeSeparated( ))
-
 function toBeSeperated(words) {
     const wordArray = words.split(" ");
     return wordArray;
